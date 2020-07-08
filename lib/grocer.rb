ebd1b04c843ba00e9e7f65ee6ff92be776dc6a0e
Romina Velarde <rominapaola.velarde@gmail.com>
2020-07-08 22:28:27 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,25 +3,27 @@ def find_item_by_name_in_collection(name, collection)
 
 counter = 1 
 while counter < collection.length
-collection[counter][:item] == name
-end 
-return nil 
- 
+  if collection[counter][:item] == name
+  return collection[counter]
 
+    end 
 counter += 1
-end
+  end
+ end
  
-# filtered_array = []
-#   cart.each do |item|
-#     target_item = find_item_by_name_in_collection(item[:item], filtered_array)
-#     if target_item
-#       target_item[:count] += 1
-#     else
-#       item[:count] = 1
-#       filtered_array << item
-#     end
-#   end
-#   return filtered_array
+def consolidate_cart(cart)
+  filtered_array = []
+  cart.each do |item|
+    target_item = find_item_by_name_in_collection(item[:item], filtered_array)
+    if target_item
+      target_item[:count] += 1
+    else
+      item[:count] = 1
+      filtered_array << item
+    end
+  end
+  return filtered_array
+end
 
 
   
