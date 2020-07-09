b01fcd5228bc873f6308b3fcee2c282e66c92298
Romina Velarde <rominapaola.velarde@gmail.com>
2020-07-09 00:18:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,24 +12,24 @@ counter += 1
  end
  
 def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-  
-  while index < cart.length do
-    item = find_item_by_name_in_collection(cart[index][:item], new_cart)
-    if item
-      new_cart_index = 0
-      while new_cart_index < new_cart.length do
-        if new_cart[new_cart_index][:item] === item[:item]
-          new_cart[new_cart_index][:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      cart[index][:count] = 1
-      new_cart << cart[index]
-    end
-    index += 1
-  end
-  new_cart
-end
+#   index = 0
+#   new_cart = []
+#  
+#   cart.each do |grocery_item|
+#     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+#     if current_item
+#       new_cart_index = 0
+#       new_cart.each do |new_cart_item|
+#         if new_cart_item[:item] === current_item[:item]
+#           new_cart_item[:count] += 1
+#         end
+#         new_cart_index += 1
+#       end
+#     else
+#       grocery_item[:count] = 1
+#       new_cart << grocery_item
+#     end
+#     index += 1
+#   end
+#   new_cart
+# aend
